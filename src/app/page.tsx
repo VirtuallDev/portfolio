1f2128b382335496e332fb2a34790b075a9cc091
Virtual <nitaystain090@gmail.com>
2023-09-17 22:11:07 +0300

    Added footer

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <section className="w-full h-full">
+      Hello, World!
+    </section>
+  )
+}
---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+
+
+const Footer = () => (
+    <footer className="fixed bottom-0 bg-indigo-500 text-indigo-50 p-1 w-screen z-50 text-center">
+        Made By VirtuallDev
+    </footer>
+)
+
+export default Footer;
